65a0d0d606e58217fa15743a784a5072a1774f78
JeremyMikaleff <jeremy.mikaleff@gmail.com>
2024-09-09 23:20:31 +0200

    build(front-anderson): modification du port exposé et ajout d'un .bat

---
+++ install_anderson_front.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: Pull l'image depuis GitHub Container Registry
+docker pull ghcr.io/projets-finaux-simplon-2024/front-anderson:main
+
+:: Supprimer le conteneur existant s'il existe déjà
+docker rm -f container_anderson_front
+
+:: Run le conteneur avec les variables d'environnement et le mapping de port
+docker run -d -p 3000:3000 --name container_anderson_front -e REACT_APP_BACK_API_URL="http://localhost:8080" ghcr.io/projets-finaux-simplon-2024/front-anderson:main
+
+echo Le conteneur container_anderson_front est démarré et écoute sur le port 3000.
